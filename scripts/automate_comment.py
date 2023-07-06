a3437a9f107591638cd3d45b5feff89e3dd5e970
krushna1512 <87175511+krushna1512@users.noreply.github.com>
2023-07-06 10:33:10 +0530

    Update automate_comment.py

--- scripts/automate_comment.py
+++ scripts/automate_comment.py
@@ -9,7 +9,15 @@ def comment_on_inactive_issues():
 
     g = Github(github_token)
     repo = g.get_repo(repository)
-    issues = repo.get_issues(state=['open', 'In Process'])
+
+    # Retrieve issues with state='open'
+    open_issues = repo.get_issues(state='open')
+
+    # Retrieve issues with state='inprocess'
+    inprocess_issues = repo.get_issues(state='inprocess')
+
+    # Combine the issues into a single list
+    issues = list(open_issues) + list(inprocess_issues)
 
     current_date = datetime.now() - timedelta(minutes=4)
 
@@ -21,7 +29,7 @@ def comment_on_inactive_issues():
             last_comment = comment
 
         if not last_comment or last_comment.created_at < current_date:
-            comment = "Hello! It seems no activity has been recorded on this issue for the past 4 days. "
+            comment = "Hello! It seems no activity has been recorded on this issue for the past 4 days."
             issue.create_comment(comment)
 
 comment_on_inactive_issues()
